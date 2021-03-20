917230e782c8531908a6b26bd7e583199500afba
Salesdata448 <78691618+Salesdata448@users.noreply.github.com>
2021-03-20 07:57:01 +0000

    Test Commit 1

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+data=pd.read_csv("netflix_titles.csv")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+print(data.head(5), data.shape)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
