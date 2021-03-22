7218b753d5eef6c97f1ae1fe59c3619aa37b014b
Salesdata448 <78691618+Salesdata448@users.noreply.github.com>
2021-03-22 20:22:06 +0000

    I have changed dataset from Netflix to Apple for the project

--- Apple.py
+++ Apple.py
@@ -1,9 +1,15 @@
 import pandas as pd
+
 app_data = pd.read_csv('AppleStore.csv')
 
 print(app_data)
-print(app_data.head(5))
+print(app_data.head(5),app_data.shape)
 #Apple dataset imported as csv from Kaggle
+
+
+
+
+
 
 
 
