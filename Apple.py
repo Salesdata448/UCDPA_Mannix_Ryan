e83cc96d063091e6b2846f8f00ba814aff2ac298
Salesdata448 <78691618+Salesdata448@users.noreply.github.com>
2021-03-22 20:48:10 +0000

    #dropping rows where data is missing

--- Apple.py
+++ Apple.py
@@ -3,9 +3,17 @@ import pandas as pd
 app_data = pd.read_csv('AppleStore.csv')
 
 print(app_data)
-print(app_data.head(5),app_data.shape)
+print(app_data.shape)
 #Apple dataset imported as csv from Kaggle
 
+missing_val=app_data.isnull().sum
+
+print(missing_val)
+#no missing values
+
+#dropping rows where data is missing
+droprows=app_data.dropna()
+print(app_data.shape,droprows.shape)
 
 
 
