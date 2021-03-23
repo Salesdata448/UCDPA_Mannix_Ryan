6a182e9cbab146f00d33a2d232072b23d99d0b67
Salesdata448 <78691618+Salesdata448@users.noreply.github.com>
2021-03-23 19:24:36 +0000

    Commit

--- Apple.py
+++ Apple.py
@@ -1,27 +1,33 @@
 import pandas as pd
+import numpy as np
 
-app_data = pd.read_csv('AppleStore.csv')
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('seaborn')
+sns.set(font_scale=1.8)
 
-print(app_data)
-print(app_data.shape)
-#Apple dataset imported as csv from Kaggle
+#read both csv's the description and the data
+df_app = pd.read_csv('AppleStore.csv')
+df_description = pd.read_csv('appleStore_description.csv')
 
-missing_val=app_data.isnull().sum
+print(df_app.isnull().sum())
 
-print(missing_val)
-#no missing values
+print(df_description.head())
 
-#dropping rows where data is missing
-droprows=app_data.dropna()
-print(app_data.shape,droprows.shape)
+# Merge datasets
 
-#no rows have missing data
+df_app['app_desc'] = df_description['app_desc']
 
-#dataset seems to be clean no duplicates to drop.
-drop_duplicates= app_data.drop_duplicates()
-print(app_data.shape,drop_duplicates.shape)
+df_app = df_app.iloc[:, 1:]
 
+print(df_app.head())
 
+df_app['size_bytes_in_MB'] = df_app['size_bytes'] / (1024 * 1024.0)
 
+df_app['isNotFree'] = df_app['price'].apply(lambda x: 1 if x > 0 else 0)
 
+df_app['isNotFree'].value_counts().plot.bar()
+plt.xlabel('IsNotFree(Free == 0, NotFree == 1)')
+plt.ylabel('No. of Apps')
+plt.show()
 
