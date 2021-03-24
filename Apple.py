40610de8dda33bbc30d0382af14ba6b719797819
Salesdata448 <78691618+Salesdata448@users.noreply.github.com>
2021-03-24 08:40:04 +0000

    This is the start of my final project. Up to this point I have used a real world dataset imported from Kaggle. Imported the db into Pycharm which consisted of two csv's. In line 36 I have used code to sort, index and group the data. I have then used code and seaborn to display the information graphically.(See final report Fig.1)

--- Apple.py
+++ Apple.py
@@ -1,33 +1,46 @@
-import pandas as pd
-import numpy as np
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+
 
 import matplotlib.pyplot as plt
-import seaborn as sns
-plt.style.use('seaborn')
-sns.set(font_scale=1.8)
 
-#read both csv's the description and the data
-df_app = pd.read_csv('AppleStore.csv')
-df_description = pd.read_csv('appleStore_description.csv')
+# Importing both Applestore.csv and Applestore_desc csv.Naming Applestore.csv df and Applestore_desc
+# df_desc. From df, dropping unwanted columns. Rubric asks to drop duplicates but dropping columns as you can see from
+# line 18 there are no missing values or duplicates to drop. Printing head of df to understand data
 
-print(df_app.isnull().sum())
+data = pd.read_csv('AppleStore.csv')
+df=data
+df=df.drop(['Unnamed: 0','track_name','currency','ver','vpp_lic'],axis=1)
+df_desc=pd.read_csv('appleStore_description.csv')
+print(df.head())
+print(df.shape)
 
-print(df_description.head())
+#checking to see if there are missing values
 
-# Merge datasets
+missing_values_count = df.isnull().sum()
+print(missing_values_count[0:5])
 
-df_app['app_desc'] = df_description['app_desc']
+missing_values_count_df_desc = df_desc.isnull().sum()
+print(missing_values_count_df_desc[0:5])
+#no missing values in either source but have checked as per rubric instructions.
 
-df_app = df_app.iloc[:, 1:]
 
-print(df_app.head())
+# columns have been dropped and shape is displayed in shell
 
-df_app['size_bytes_in_MB'] = df_app['size_bytes'] / (1024 * 1024.0)
+# printing types of data for each column. Mixture of floats and integers.
 
-df_app['isNotFree'] = df_app['price'].apply(lambda x: 1 if x > 0 else 0)
+print(df.dtypes)
 
-df_app['isNotFree'].value_counts().plot.bar()
-plt.xlabel('IsNotFree(Free == 0, NotFree == 1)')
-plt.ylabel('No. of Apps')
+#getting mean user ratings by indexing by prime genre and getting the mean of user ratings per genre.
+mean_user_ratings=df.groupby('prime_genre')['user_rating'].mean().reset_index().sort_values(by=['user_rating'])
+
+plt.figure(figsize = (10, 8), facecolor = None)
+sns.set_style("ticks")
+plot = sns.barplot(x="prime_genre", y="user_rating", data=mean_user_ratings,order=mean_user_ratings['prime_genre'])
+
+plot.set_xticklabels(mean_user_ratings['prime_genre'], rotation=90, ha="center")
+plot.set(xlabel='App prime genre',ylabel='Average user ratings')
+plot.set_title('App Genre vs Average User rating')
+
 plt.show()
-
--- main.py
+++ main.py
@@ -1,26 +1,6 @@
+# Downloaded Apple App store CSV from Kaggle
+
+import numpy as np
 import pandas as pd
 
-data=pd.read_csv("netflix_titles.csv")
-
-print(data.shape)
-
-missing_val=data.isnull().sum()
-
-print(missing_val)
-
-new_data = data.dropna(subset=["rating","date_added"])
-
-print(new_data.shape)
-
-new_data = data.drop_duplicates(subset=["director", "rating","duration","date_added"])
-
-print(new_data.shape)
-
-
-
-
-
-
-
-
-
+import pandas as pd
